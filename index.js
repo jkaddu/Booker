081a3db8b909e43f4088698faa7237a666c66906
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Pro.local>
2023-03-14 17:53:55 -0700

    about page redone

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const toggleMenu = () => {
-    menu.classList.toggle('menu--open');
-}
+// const toggleMenu = () => {
+//     menu.classList.toggle('menu--open');
+// }
 
-const menu = document.querySelector('.menu');
+// const menu = document.querySelector('.menu');
 
-const menuButton = document.querySelector('.menu-button');
+// const menuButton = document.querySelector('.menu-button');
 
-menuButton.addEventListener('click', () => {
-    toggleMenu();
-})
+// menuButton.addEventListener('click', () => {
+//     toggleMenu();
+// })
 
+const ctaBtn = document.querySelector(".ctaBtn");
+ctaBtn.addEventListener("click", function () {
+  window.open("https://determined-leakey-71b016.netlify.com/");
+});
+
+// const nav = document.querySelector("nav");
+// nav.addEventListener("scroll", function () {
+//   nav.style.backgroundColor = "red";
+// });
