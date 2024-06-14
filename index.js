1e1d59a7034ca40a593a1529a1431021face0060
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-06-14 14:18:50 -0500

    had to replace a tags

--- index.js
+++ index.js
@@ -1,59 +1,19 @@
-// const toggleMenu = () => {
-//     menu.classList.toggle('menu--open');
-// }
-
-// const menu = document.querySelector('.menu');
-
-// const menuButton = document.querySelector('.menu-button');
-
-// menuButton.addEventListener('click', () => {
-//     toggleMenu();
-// })
-
 const ctaBtn = document.querySelector(".ctaBtn");
 ctaBtn.addEventListener("click", function () {
-  window.open("https://determined-leakey-71b016.netlify.com/");
+  window.open("503.html");
 });
 
 const ctaBtn2 = document.querySelector(".ctaBtn2");
-
 ctaBtn2.addEventListener("click", function () {
-  window.open("https://determined-leakey-71b016.netlify.com/");
+  window.open("503.html");
 });
 
 const btnMid = document.querySelector(".btn-mid");
-
 btnMid.addEventListener("click", function () {
-  window.open("https://determined-leakey-71b016.netlify.com/");
+  window.open("503.html");
 });
 
 const btnBot = document.querySelector(".btn-bot");
-
 btnBot.addEventListener("click", function () {
-  window.open("https://determined-leakey-71b016.netlify.com/");
+  window.open("503.html");
 });
-
-// const nav = document.querySelector("nav");
-// nav.addEventListener("scroll", function () {
-//   nav.style.backgroundColor = "red";
-// });
-
-// Reveal Content
-// const allSections = document.querySelectorAll(".section");
-
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove("hidden-section");
-//   observer.unobserve(entry.target);
-// };
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   threshold: 0.05,
-// });
-
-// allSections.forEach(function (section) {
-//   sectionObserver.observe(section);
-//   section.classList.add("hidden-section");
-// });
